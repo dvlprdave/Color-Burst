64311293478a53c221cfae99f6f78b8381deda61
David <devdavee@gmail.com>
2019-08-12 21:16:55 -0400

    Added PaletteList and links

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React from 'react';
+// import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Palette from './components/Palette/Palette';
+import PaletteList from './components/PaletteList/PaletteList';
 import seedColors from './seedColors'
 import { generatePalette } from './colorHelpers'
 
-
-function App() {
-  return (
-    <Switch>
-      <Route exact path='/' />
-      <Route exact path='/palette/:id' />
-    </Switch>
-    // <div>
-    //   <Palette palette={generatePalette(seedColors[4])} />
-    // </div >
-  );
+class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function (palette) {
+      return palette.id === id;
+    });
+  }
+  render() {
+    return (
+      <Switch>
+        <Route
+          exact
+          path='/'
+          render={() => <PaletteList palettes={seedColors} />}
+        />
+        <Route
+          exact
+          path='/palette/:id'
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+    );
+  }
 }
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import Select from '@material-ui/core/Select'
 import MenuItem from '@material-ui/core/MenuItem'
 import Slider from 'rc-slider'
@@ -34,7 +35,7 @@ class Navbar extends Component {
     return (
       <nav className='Navbar'>
         <div className='logo'>
-          <a href='/'>Color-Burst</a>
+          <Link to='/'>Color-Burst</Link>
         </div>
         <div className='slider-container'>
           <span>Level: {level}</span>
