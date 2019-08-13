69be3737695cbc47ed3dd26dbe3b4540d6097cf7
David <devdavee@gmail.com>
2019-08-12 21:41:58 -0400

    Added Styled-Components and MiniPalette

---
+++ src/components/MiniPalette.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Main = styled.div`
+  background: purple;
+`
+
+const MiniPalette = (props) => (
+  <Main>
+    <h1>Mini Palette</h1>
+  </Main>
+)
+
+export default MiniPalette
