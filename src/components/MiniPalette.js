92fc0ae5abe00c7bb14ee2307ce864c65baf3671
David <devdavee@gmail.com>
2019-08-14 20:20:54 -0400

    Styling MiniPalette with styled components

--- src/components/MiniPalette.js
+++ src/components/MiniPalette.js
@@ -2,13 +2,45 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Main = styled.div`
-  background: purple;
+  background: white;
+  border: 1px solid black;
+  border-radius: 5px;
+  position: relative;
+  overflow: hidden;
+
+   &:hover {
+    cursor: pointer;
+   }
 `
 
-const MiniPalette = (props) => (
-  <Main>
-    <h1>Mini Palette</h1>
-  </Main>
-)
+const Colors = styled.div`
+  background-color: grey;
+`
+
+const Title = styled.h5`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  margin: 0;
+  color: black;
+  padding-top: 0.5rem;
+  font-size: 1rem;
+`
+
+const Emoji = styled.span`
+  font-size: 1.5rem;
+  margin-left: 0.5rem;
+`
+
+const MiniPalette = ({ paletteName, emoji }) => {
+  // const { paletteName, emoji } = props
+  return (
+    <Main>
+      <Colors></Colors>
+      <Title>{paletteName} <Emoji>{emoji}</Emoji></Title>
+    </Main>
+  )
+}
 
 export default MiniPalette
--- src/components/PaletteList/PaletteList.js
+++ src/components/PaletteList/PaletteList.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 import MiniPalette from '../MiniPalette';
 
 class PaletteList extends Component {
@@ -7,12 +6,8 @@ class PaletteList extends Component {
     const { palettes } = this.props
     return (
       <div>
-        <MiniPalette />
-        <h1>React Colors</h1>
         {palettes.map(palette => (
-          <p>
-            <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-          </p>
+          <MiniPalette {...palette} />
         ))}
       </div>
     );
